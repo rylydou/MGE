a4d793eccdcd3a83e9b6196608ddb86ae0653402
CiberTurtle <58660466+CiberTurtle@users.noreply.github.com>
2021-08-30 17:03:08 -0500

    Adding MemlDotNet

--- MGE/Serialization/PropAttribute.cs
+++ MGE/Serialization/PropAttribute.cs
@@ -3,12 +3,12 @@ using System;
 namespace MGE.Serialization
 {
 	[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
-	sealed class ParamAttribute : Attribute
+	sealed class PropAttribute : Attribute
 	{
 		public string name { get; init; }
 		public int order { get; init; }
 
-		public ParamAttribute(string name = null, int order = 0)
+		public PropAttribute(string name = null, int order = 0)
 		{
 			this.name = name;
 			this.order = order;
