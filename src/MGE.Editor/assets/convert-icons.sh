a5dc01bf52b615cfc23814fc3d301d473b4836ac
Ciber_Turtle <rylydou@gmail.com>
2021-10-13 22:15:23 -0500

    Higher res icons

--- src/MGE.Editor/assets/convert-icons.sh
+++ src/MGE.Editor/assets/convert-icons.sh
@@ -1,14 +1,15 @@
 IN=icons-svg
 OUT=icons
-RES=16x16 # Maybe try 32x32 or 64x64?
+RES=64x64
 
 echo "Cleaning OUT"
-rm $OUT\*
+rm $OUT/*
 
-for f in $IN\*
+for f in $IN/*
 do
 	echo "Converting $f"
-	gtk-encode-symbolic-svg $f $RES -o icons-symbolic
+	gtk-encode-symbolic-svg $f $RES -o $OUT
 done
 
+echo "Done!"
 read
