73e725c71cfb0e6723bf4278ad4f9178d14a64ff
Ciber_Turtle <rylydou@gmail.com>
2021-11-04 20:23:19 -0500

    Moved projects out of the`src` folder

---
+++ MGE/GameWindow.cs
@@ -0,0 +1,41 @@
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace MGE
+{
+	public class GameWindow : OpenTK.Windowing.Desktop.GameWindow
+	{
+		public GameWindow() : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = new Vector2i(800, 600), Title = "LearnOpenTK - Creating a Window", })
+		{
+		}
+
+		protected override void OnLoad()
+		{
+			base.OnLoad();
+
+			GL.ClearColor(0f, 0.1f, 0.025f, 1f);
+		}
+
+		protected override void OnUpdateFrame(FrameEventArgs e)
+		{
+			base.OnUpdateFrame(e);
+
+			if (KeyboardState.IsKeyDown(Keys.Escape))
+			{
+				Close();
+			}
+		}
+
+		protected override void OnRenderFrame(FrameEventArgs args)
+		{
+			base.OnRenderFrame(args);
+
+			GL.Clear(ClearBufferMask.ColorBufferBit);
+
+			SwapBuffers();
+		}
+	}
+}
---
+++ MGE/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MGE
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
