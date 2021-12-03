36211dca0d0afb9e0dfdba4a0ee34919da1a025c
CiberTurtle <58660466+CiberTurtle@users.noreply.github.com>
2021-12-03 15:18:59 -0600

    Minor fixes

--- MGE/Common/RNG.cs
+++ MGE/Common/RNG.cs
@@ -1,42 +1,41 @@
 using System;
 
-namespace MGE
+namespace MGE;
+
+public class RNG
 {
-	public class RNG
+	public readonly static RNG shared = new();
+
+	int _state;
+
+	public RNG() : this(Math.Abs(Guid.NewGuid().GetHashCode())) { }
+	public RNG(int seed)
 	{
-		public readonly static RNG shared = new();
-
-		int _state;
-
-		public RNG() : this(Math.Abs(Guid.NewGuid().GetHashCode())) { }
-
-		public RNG(int seed)
-		{
-			if (seed < 1) throw new ArgumentOutOfRangeException(nameof(seed), "Seed must be greater than zero");
-
-			_state = seed;
-		}
-
-		public int RandomInt() => ((214013 * _state + 2531011) >> 16) & 0x7FFF;
-		public int RandomInt(int max) => (int)(max * RandomFloat() + 0.5f);
-		public int RandomInt(int min, int max) => (int)((max - min) * RandomFloat() + 0.5f) + min;
-
-		public float RandomFloat() => RandomInt() / (float)short.MaxValue;
-		public float RandomFloat(float max) => max * RandomFloat();
-		public float RandomFloat(float min, float max) => (max - min) * RandomFloat() + min;
-
-		public bool RandomBool() => RandomInt(1) == 0;
-
-		public int RandomSign() => RandomBool() ? 1 : -1;
-
-		public float RandomAngle() => RandomFloat(Math.pi2);
-
-		public Vector2 RandomUnitVector()
-		{
-			var angle = RandomAngle();
-			return new((float)Math.Cos(angle), (float)Math.Sin(angle));
-		}
-
-		public Color RandomColor() => new(RandomFloat(), RandomFloat(), RandomFloat());
+		if (seed < 1) throw new ArgumentOutOfRangeException(nameof(seed), "Seed must be greater than zero");
+		_state = seed;
 	}
+
+	public int RandomInt() => ((214013 * _state + 2531011) >> 16) & 0x7FFF;
+	public int RandomInt(int max) => (int)(max * RandomFloat() + 0.5f);
+	public int RandomInt(int min, int max) => (int)((max - min) * RandomFloat() + 0.5f) + min;
+
+	public float RandomFloat() => RandomInt() / (float)short.MaxValue;
+	public float RandomFloat(float max) => max * RandomFloat();
+	public float RandomFloat(float min, float max) => (max - min) * RandomFloat() + min;
+
+	public bool RandomBool() => RandomInt(1) == 0;
+	public bool RandomBool(float chance) => RandomFloat() <= chance;
+	public bool RandomBool(int chance) => RandomInt(100) <= chance;
+
+	public int RandomSign() => RandomBool() ? 1 : -1;
+
+	public float RandomAngle() => RandomFloat(Math.pi2);
+
+	public Vector2 RandomUnitVector()
+	{
+		var angle = RandomAngle();
+		return new((float)Math.Cos(angle), (float)Math.Sin(angle));
+	}
+
+	public Color RandomColor() => new(RandomFloat(), RandomFloat(), RandomFloat());
 }
--- MGE/GameWindow.cs
+++ MGE/GameWindow.cs
@@ -2,6 +2,7 @@ using System;
 using MGE.Graphics;
 using OpenTK.Graphics.OpenGL;
 using OpenTK.Windowing.Common;
+using tainicom.Aether.Physics2D.Dynamics;
 
 namespace MGE;
 
@@ -12,6 +13,14 @@ public class GameWindow : OpenTK.Windowing.Desktop.GameWindow
 
 	double updateTime;
 	double renderTime;
+
+	World _world;
+
+	Body _ballBody;
+	Fixture _ballFixture;
+
+	Body _groundBody;
+	Fixture _groundFixture;
 
 	Texture sprite;
 
@@ -25,6 +34,17 @@ public class GameWindow : OpenTK.Windowing.Desktop.GameWindow
 		CenterWindow(new(320 * 4, 180 * 4));
 		Focus();
 
+		_world = new(gravity: new(0, -64));
+		_ballBody = _world.CreateBody(bodyType: BodyType.Dynamic);
+		_ballFixture = _ballBody.CreateCircle(64f, 64f);
+		_ballFixture.Restitution = 0.3f;
+		_ballFixture.Friction = 0.5f;
+
+		_groundBody = _world.CreateBody();
+		_groundFixture = _groundBody.CreateRectangle(320, 90, 1, new(0, -320));
+		_groundFixture.Restitution = 0.3f;
+		_groundFixture.Friction = 0.5f;
+
 		Scene.root.AttachNode(new SpriteNode(Texture.LoadTexture("Tree.png")));
 	}
 
@@ -34,8 +54,8 @@ public class GameWindow : OpenTK.Windowing.Desktop.GameWindow
 
 		GL.Enable(EnableCap.Blend);
 		GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
-		// GL.Enable(EnableCap.Multisample);
-		GL.Disable(EnableCap.Multisample);
+		GL.Enable(EnableCap.Multisample);
+		// GL.Disable(EnableCap.Multisample);
 	}
 
 	protected override void OnUnload()
@@ -69,6 +89,8 @@ public class GameWindow : OpenTK.Windowing.Desktop.GameWindow
 
 		updateTime = args.Time;
 
+		_world.Step((float)args.Time);
+
 		Scene.Update();
 	}
 
@@ -87,6 +109,9 @@ public class GameWindow : OpenTK.Windowing.Desktop.GameWindow
 		Font.current.DrawText($"Update: {1f / updateTime:F0}fps ({updateTime * 1000:F2}ms) Render: {1f / renderTime:F0}fps ({renderTime * 1000:F2}ms)", new(-Size.X / 2 + 4, -Size.Y / 2 + 4));
 
 		GFX.DrawTextureRegion(sprite, new(256, 256, 32, 32), new(0, 0, sprite.size));
+
+		GFX.DrawCircleFilled(new(_ballBody.Position.X, _ballBody.Position.Y), 64, new(0, 1, 0, 0.9f));
+		GFX.DrawLine(new Vector2(_ballBody.Position.X, _ballBody.Position.Y), new Vector2(_ballBody.Position.X, _ballBody.Position.Y) + Vector2.RotateAroundPoint(new(0, 64), _ballBody.Rotation), 2, Color.white);
 
 		GFX.Flush();
 
