d7d07e444ba01ada5898470d641427296182a89f
Ciber_Turtle <rylydou@gmail.com>
2022-04-18 21:10:20 -0500

    Snow

--- Demo/Game.cs
+++ Demo/Game.cs
@@ -9,6 +9,54 @@ public class Game : Module
 
 	FrameBuffer _framebuffer = new(320 * 2, 180 * 2);
 
+	struct Particle
+	{
+		public static Vector2 size;
+		public static Color color = new(0xdff6f5);
+		public static Color colorAlt = new(0x8aebf1);
+
+		public Vector2 position;
+		public Vector2 velocity;
+		public float lifetime;
+		public bool alt;
+
+		public void Start()
+		{
+			velocity = new(RNG.shared.RandomFloat(-48, 48), RNG.shared.RandomFloat(48, 192));
+			position = new(RNG.shared.RandomFloat(size.x), -RNG.shared.RandomFloat(size.y));
+			lifetime = 0;
+			alt = RNG.shared.RandomBool();
+		}
+
+		public void Update(float delta)
+		{
+			lifetime += delta;
+			position += velocity * delta;
+
+			if (position.y > size.y)
+			{
+				Start();
+			}
+		}
+
+		public void Render()
+		{
+			Batch2D.current.Rect(position, Vector2.one, alt ? colorAlt : color);
+		}
+	}
+
+	Particle[] _particles;
+
+	public Game()
+	{
+		Particle.size = new(_framebuffer.renderWidth, _framebuffer.renderHeight);
+		_particles = new Particle[2048];
+		for (int i = 0; i < _particles.Length; i++)
+		{
+			_particles[i].Start();
+		}
+	}
+
 	protected override void Startup()
 	{
 		player.floorY = _framebuffer.renderHeight - 24;
@@ -35,10 +83,19 @@ public class Game : Module
 			return;
 		}
 
-		Batch2D.current.CheckeredPattern(new(0, 0, App.window.size.x, App.window.size.y), 12, 12, Color.darkGray, Color.gray);
+		var topColor = new Color(0x3978a8);
+		var bottomColor = new Color(0x394778);
+		Batch2D.current.Rect(new(_framebuffer.renderWidth, _framebuffer.renderHeight), topColor, topColor, bottomColor, bottomColor);
+
+		// Batch2D.current.CheckeredPattern(new(0, 0, App.window.size.x, App.window.size.y), 12, 12, Color.darkGray, Color.gray);
 
 		player.onUpdate(delta);
 		playerAlt.onUpdate(delta);
+
+		for (int i = 0; i < _particles.Length; i++)
+		{
+			_particles[i].Update(delta / 4);
+		}
 	}
 
 	protected override void Tick(float delta)
@@ -49,6 +106,11 @@ public class Game : Module
 
 	void Render(Window window)
 	{
+		for (int i = 0; i < _particles.Length; i++)
+		{
+			_particles[i].Render();
+		}
+
 		Batch2D.current.Render(_framebuffer);
 		Batch2D.current.Clear();
 
